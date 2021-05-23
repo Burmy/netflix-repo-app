be1f699223714d3b58833f92180f205c0bfeaac6
Burmy <62005549+Burmy@users.noreply.github.com>
2021-05-23 13:50:45 -0700

    made commits.html

--- js/home.js
+++ js/home.js
@@ -11,7 +11,7 @@ if (mainDiv) {
       });
       repos.forEach(function (data) {
         output += `
-        <a href="https://api.github.com/repos/Netflix/${data.name}/commits">
+        <a onclick=repoSelected('${data.name}') onclick=getCommit()>
                 <div class="repo-card" id=${data.id} ">
                     <div class="repo-name">${data.name}</div>
                     <div class="repo-desc">${data.description}</div>
@@ -28,42 +28,37 @@ if (mainDiv) {
     });
 }
 
-// if (mainDiv) {
-//   let fetchURL = "https://api.github.com/orgs/Netflix/repos";
-//   fetch(fetchURL)
-//     .then((response) => response.json())
-//     .then((repos) => {
-//       let output = "";
-//       repos.sort(function (a, b) {
-//         return parseInt(b.forks_count) - parseInt(a.forks_count);
-//       });
-//       repos.forEach(function (data) {
-//         output += `
-//         <a href="https://api.github.com/repos/Netflix/${data.name}/commits">
-//                 <div class="repo-card" id=${data.id} ">
-//                     <div class="repo-name">${data.name}</div>
-//                     <div class="repo-desc">${data.description}</div>
-//                     <div class="repo-info">
-//                         <div class="repo-lang">Language: ${data.language}</div>
-//                         <div class="repo-star"> <ion-icon name="star"></ion-icon> Stars: ${data.stargazers_count}</div>
-//                         <div class="repo-fork"><ion-icon name="git-network"></ion-icon> Forks: ${data.forks_count}</div>
-//                     </div>
-//                     <div class="repo-created">Created on: ${data.created_at}</div>
-//                 </div>
-//         </a>`;
-//         fetch(`https://api.github.com/repos/Netflix/${data.name}/commits`)
-//           .then((response) => response.json())
-//           .then((commits) => {
-//             let commit_output = "";
-//             commits.forEach(function (data) {
-//               commit_output += `
-//               <div class="repo-name">${data.commit.message}</div>
-//               `;
-//             });
-//             document.getElementById("container").innerHTML = commit_output;
-//           });
-//       });
+function repoSelected(name) {
+  sessionStorage.setItem("repoName", name);
+  window.location = "commits.html";
+  return false;
+}
 
-//       document.getElementById("home-container").innerHTML = output;
-//     });
-// }
+function getCommit() {
+  let repoName = sessionStorage.getItem("repoName");
+  let fetchURL = "https://api.github.com/repos/Netflix/" + repoName + "/commits";
+  fetch(fetchURL)
+    .then((response) => response.json())
+    .then((repos) => {
+      let output = "";
+      repos.sort(function (a, b) {
+        return parseInt(b.forks_count) - parseInt(a.forks_count);
+      });
+      repos.forEach(function (data) {
+        output += `
+        <a onclick=repoSelected('${data.name}') onclick=getCommit()>
+                <div class="repo-card" id=${data.node_id} ">
+                    <div class="repo-name">${data.node_id}</div>
+                    <div class="repo-desc">${data.description}</div>
+                    <div class="repo-info">
+                        <div class="repo-lang">Language: ${data.language}</div>
+                        <div class="repo-star"> <ion-icon name="star"></ion-icon> Stars: ${data.stargazers_count}</div>
+                        <div class="repo-fork"><ion-icon name="git-network"></ion-icon> Forks: ${data.forks_count}</div>
+                    </div>
+                    <div class="repo-created">Created on: ${data.created_at}</div>
+                </div>
+        </a>`;
+      });
+      document.getElementById("container").innerHTML = output;
+    });
+}
