3a1efbffbf88ae3560c709a4149fc3b6e1fd4191
Burmy <62005549+Burmy@users.noreply.github.com>
2021-05-23 17:47:43 -0700

    made commit list page

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-let mainDiv = document.getElementById("home-container");
+let mainDiv = document.getElementById("repo-container");
 
 if (mainDiv) {
   let fetchURL = "https://api.github.com/orgs/Netflix/repos";
@@ -10,21 +10,25 @@ if (mainDiv) {
         return parseInt(b.forks_count) - parseInt(a.forks_count);
       });
       repos.forEach(function (data) {
+        createdOn = new Date(data.created_at).toDateString();
         output += `
         <a onclick=repoSelected('${data.name}') onclick=getCommit()>
                 <div class="repo-card" id=${data.id} ">
                     <div class="repo-name">${data.name}</div>
                     <div class="repo-desc">${data.description}</div>
                     <div class="repo-info">
+                      <div class="repo-sub-info">
                         <div class="repo-lang">Language: ${data.language}</div>
                         <div class="repo-star"> <ion-icon name="star"></ion-icon> Stars: ${data.stargazers_count}</div>
                         <div class="repo-fork"><ion-icon name="git-network"></ion-icon> Forks: ${data.forks_count}</div>
+                      </div>
+                        <div class="repo-created">Created on: ${createdOn}</div>
                     </div>
-                    <div class="repo-created">Created on: ${data.created_at}</div>
+                    
                 </div>
         </a>`;
       });
-      document.getElementById("home-container").innerHTML = output;
+      document.getElementById("repo-container").innerHTML = output;
     });
 }
 
@@ -39,26 +43,19 @@ function getCommit() {
   let fetchURL = "https://api.github.com/repos/Netflix/" + repoName + "/commits";
   fetch(fetchURL)
     .then((response) => response.json())
-    .then((repos) => {
+    .then((commits) => {
       let output = "";
-      repos.sort(function (a, b) {
-        return parseInt(b.forks_count) - parseInt(a.forks_count);
-      });
-      repos.forEach(function (data) {
+      commits.forEach(function (data) {
+        createdOn = new Date(data.commit.author.date).toDateString();
         output += `
-        <a onclick=repoSelected('${data.name}') onclick=getCommit()>
-                <div class="repo-card" id=${data.node_id} ">
-                    <div class="repo-name">${data.node_id}</div>
-                    <div class="repo-desc">${data.description}</div>
-                    <div class="repo-info">
-                        <div class="repo-lang">Language: ${data.language}</div>
-                        <div class="repo-star"> <ion-icon name="star"></ion-icon> Stars: ${data.stargazers_count}</div>
-                        <div class="repo-fork"><ion-icon name="git-network"></ion-icon> Forks: ${data.forks_count}</div>
-                    </div>
-                    <div class="repo-created">Created on: ${data.created_at}</div>
-                </div>
-        </a>`;
+          <div class="commit-list">
+              <div class="">${data.commit.message}</div>
+              <div class="commit-info">
+                <div class="">${data.commit.author.name} on ${createdOn}</div>
+                <div class="commit-hash">Commit Hash: ${data.sha}</div>
+              </div>
+          </div>`;
       });
-      document.getElementById("container").innerHTML = output;
+      document.getElementById("commit-container").innerHTML = output;
     });
 }
